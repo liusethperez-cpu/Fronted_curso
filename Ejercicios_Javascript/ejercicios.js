4f47adb97c2d3a69f42c9b76768bf97300847f64
Liuseth <liuseth.perez@gmail.com>
2025-10-13 13:58:55 +0200

    Test

---
+++ Ejercicios_Javascript/ejercicios.js
@@ -0,0 +1,42 @@
+
+//let vacio = []; // un array(lista) vacio
+//let colores = ["rojo", "azul", "verde"];// tipo de dato string(cadena de caracteres)
+//let numeros = [ 10, 20, 30, 40, 50];// tipo de dato integer(entero)
+//let mezclado = [true, "hola", 5]; // diferentes tipos de datos, true es boolean
+
+//console.log(colores[1][2]) // muestra el primer elemento del array, en este caso "rojo"
+//console.log(colores.length) // muestra la longitud del array, en este caso 3
+//let compra = ["pan", "leche", "huevos"];
+//compra.push("huevos"); //añadimis un elemento a la lista
+//console.log(compra) //agregamos un elemento a la lista
+//compra.pop(); // elimina el ultimo elemento de la lista 
+//compra.shift(); // elimina el primer elemento de la lista
+//console.log(compra) //eliminamos el ultimo elemento de la lista 
+
+//let tareasPendientes = ["estudiar JavaScript", "sacar la basura", "hacer la cama"];
+//let ultimaTarea = tareasPendientes.pop(); // elimina el ultimo elemento de la lista y lo guarda en una variable
+//console.log(`Tarea completada: ${ultimaTarea}`); // muestra la tarea completada
+
+
+//creamos funcion para agregar tarea
+//function saludar(nombre) {
+// console.log("¡Hola! " + nombre + ", bienvenido a la clase de JavaScript");
+
+//saludar("Anto");
+
+//creamos funcion flecha (arrow function)
+//const sumar = (num1, num2) => {
+    //return num1 + num2;
+}
+//const resultado = sumar(5, 10);
+//console.log("la suma es: " + resultado);
+
+
+const sumar=() => {
+     num1 = 398
+    num2 = 220
+    return num1 + num2;
+     // si la funcion tiene una sola linea de codigo, no es necesario usar llaves ni return
+}
+   
+
